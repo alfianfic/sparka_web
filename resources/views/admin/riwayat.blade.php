26dbcc4cac8b05d41e8b8972d9d8a0dcf3850b6c
alfianfic <alfiac999@gmail.com>
2025-05-15 09:17:55 +0700

    Setup awal

---
+++ resources/views/admin/riwayat.blade.php
@@ -0,0 +1,145 @@
+@extends('layout.main')
+
+@section('content')
+    <div class="col-12">
+        <div class="card my-4">
+            <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
+                <div class="bg-gradient-dark shadow-dark border-radius-lg pt-4 pb-3">
+                    <h6 class="text-white text-capitalize ps-3">NAMA PASIEN : JOHN MICHAEL</h6>
+                </div>
+            </div>
+            <div class="card-body px-0 pb-2">
+                <div class="table-responsive p-0">
+                    <table class="table align-items-center mb-0">
+                        <thead>
+                            <tr>
+                                <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Waktu
+                                    Pengujian</th>
+                                <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">FEV1
+                                </th>
+                                <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                    FVC</th>
+                                <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                    CO</th>
+                                <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">Status
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>
+                                    <div class="d-flex px-2 py-1">
+                                        <div class="d-flex flex-column justify-content-center">
+                                            <h6 class="ps-2 mb-0 text-sm">24/05/2023</h6>
+                                            <!-- <p class="text-xs text-secondary mb-0">john@creative-tim.com</p> -->
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>
+                                    <p class="text-xs font-weight-bold mb-0">0.45</p>
+                                </td>
+                                <td class="align-middle text-center">
+                                    <p class="text-xs font-weight-bold mb-0">0.7</p>
+                                </td>
+                                <td class="align-middle text-center">
+                                    <span class="text-secondary text-xs font-weight-bold">2</span>
+                                </td>
+                                <td class="align-middle">
+                                    <span class="text-secondary text-xs font-weight-bold">Gejala Sesak Nafas</span>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <div class="d-flex px-2 py-1">
+                                        <div class="d-flex flex-column justify-content-center">
+                                            <h6 class="ps-2 mb-0 text-sm">24/05/2023</h6>
+                                            <!-- <p class="text-xs text-secondary mb-0">john@creative-tim.com</p> -->
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>
+                                    <p class="text-xs font-weight-bold mb-0">0.45</p>
+                                </td>
+                                <td class="align-middle text-center">
+                                    <p class="text-xs font-weight-bold mb-0">0.7</p>
+                                </td>
+                                <td class="align-middle text-center">
+                                    <span class="text-secondary text-xs font-weight-bold">2</span>
+                                </td>
+                                <td class="align-middle">
+                                    <span class="text-secondary text-xs font-weight-bold">Gejala Sesak Nafas</span>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <div class="d-flex px-2 py-1">
+                                        <div class="d-flex flex-column justify-content-center">
+                                            <h6 class="ps-2 mb-0 text-sm">24/05/2023</h6>
+                                            <!-- <p class="text-xs text-secondary mb-0">john@creative-tim.com</p> -->
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>
+                                    <p class="text-xs font-weight-bold mb-0">0.45</p>
+                                </td>
+                                <td class="align-middle text-center">
+                                    <p class="text-xs font-weight-bold mb-0">0.7</p>
+                                </td>
+                                <td class="align-middle text-center">
+                                    <span class="text-secondary text-xs font-weight-bold">2</span>
+                                </td>
+                                <td class="align-middle">
+                                    <span class="text-secondary text-xs font-weight-bold">Gejala Sesak Nafas</span>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <div class="d-flex px-2 py-1">
+                                        <div class="d-flex flex-column justify-content-center">
+                                            <h6 class="ps-2 mb-0 text-sm">24/05/2023</h6>
+                                            <!-- <p class="text-xs text-secondary mb-0">john@creative-tim.com</p> -->
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>
+                                    <p class="text-xs font-weight-bold mb-0">0.45</p>
+                                </td>
+                                <td class="align-middle text-center">
+                                    <p class="text-xs font-weight-bold mb-0">0.7</p>
+                                </td>
+                                <td class="align-middle text-center">
+                                    <span class="text-secondary text-xs font-weight-bold">2</span>
+                                </td>
+                                <td class="align-middle">
+                                    <span class="text-secondary text-xs font-weight-bold">Gejala Sesak Nafas</span>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <div class="d-flex px-2 py-1">
+                                        <div class="d-flex flex-column justify-content-center">
+                                            <h6 class="ps-2 mb-0 text-sm">24/05/2023</h6>
+                                            <!-- <p class="text-xs text-secondary mb-0">john@creative-tim.com</p> -->
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>
+                                    <p class="text-xs font-weight-bold mb-0">0.45</p>
+                                </td>
+                                <td class="align-middle text-center">
+                                    <p class="text-xs font-weight-bold mb-0">0.7</p>
+                                </td>
+                                <td class="align-middle text-center">
+                                    <span class="text-secondary text-xs font-weight-bold">2</span>
+                                </td>
+                                <td class="align-middle">
+                                    <span class="text-secondary text-xs font-weight-bold">Gejala Sesak Nafas</span>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/user', function () {
+    return view('admin.user');
+});
+Route::get('/riwayat', function () {
+    return view('admin.riwayat');
+});
+Route::get('/home', function () {
+    return view('user.home');
+});
