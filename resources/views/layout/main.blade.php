5a8c856c2383d4c63e17f62e1f6787b91c04bb6a
Ariel-F-S <ferdiarel@gmail.com>
2025-10-13 21:34:49 +0700

    update semua file

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -231,6 +231,129 @@
   
 </style>
 
+ {{--Firebase Tasks--}}
+    <!-- The core Firebase JS SDK is always required and must be listed first -->
+    <script src="https://www.gstatic.com/firebasejs/7.14.6/firebase-app.js"></script>
+
+    <!-- TODO: Add SDKs for Firebase products that you want to use
+         https://firebase.google.com/docs/web/setup#available-libraries -->
+    <script src="https://www.gstatic.com/firebasejs/7.14.6/firebase-analytics.js"></script>
+    <script src="https://www.gstatic.com/firebasejs/7.14.6/firebase-database.js"></script>
+
+    <script>
+        // Initialize Firebase
+        var config = {
+            apiKey: "{{ config('AIzaSyBk0RIfTPuNx8LHOYx9XOywhw2aOYP2w7s') }}",
+            authDomain: "{{ config('ycmlg-2021.firebaseapp.com') }}",
+            databaseURL: "{{ config('services.firebase.database_url') }}",
+            projectId: "{{ config('ycmlg-2021') }}",
+            storageBucket: "{{ config('ycmlg-2021.appspot.com') }}",
+            messagingSenderId: "{{ config('342265162233') }}",
+            appId: "{{ config('1:342265162233:web:8ca129ccf0b0eda9467528') }}",
+            measurementId: "{{ config('services.firebase.measurement_id') }}"
+        };
+        firebase.initializeApp(config);
+        firebase.analytics();
+
+        var database = firebase.database();
+
+        var lastIndex = 0;
+
+        // Get Data
+        firebase.database().ref('Users/').on('value', function (snapshot) {
+            var value = snapshot.val();
+            var htmls = [];
+            $.each(value, function (index, value) {
+                if (value) {
+                    htmls.push('<tr>\
+                    <td>' + value.name + '</td>\
+                    <td>' + value.email + '</td>\
+                    <td><button data-toggle="modal" data-target="#update-modal" class="btn btn-info updateData" data-id="' + index + '">Update</button>\
+                    <button data-toggle="modal" data-target="#remove-modal" class="btn btn-danger removeData" data-id="' + index + '">Delete</button></td>\
+                </tr>');
+                }
+                lastIndex = index;
+            });
+            $('#tbody').html(htmls);
+            $("#submitUser").removeClass('desabled');
+        });
+
+        // Add Data
+        $('#submitUser').on('click', function () {
+            var values = $("#addUser").serializeArray();
+            var name = values[0].value;
+            var email = values[1].value;
+            var userID = lastIndex + 1;
+
+            console.log(values);
+
+            firebase.database().ref('Users/' + userID).set({
+                name: name,
+                email: email,
+            });
+
+            // Reassign lastID value
+            lastIndex = userID;
+            $("#addUser input").val("");
+        });
+
+        // Update Data
+        var updateID = 0;
+        $('body').on('click', '.updateData', function () {
+            updateID = $(this).attr('data-id');
+            firebase.database().ref('Users/' + updateID).on('value', function (snapshot) {
+                var values = snapshot.val();
+                var updateData = '<div class="form-group">\
+                    <label for="first_name" class="col-md-12 col-form-label">Name</label>\
+                    <div class="col-md-12">\
+                        <input id="first_name" type="text" class="form-control" name="name" value="' + values.name + '" required autofocus>\
+                    </div>\
+                </div>\
+                <div class="form-group">\
+                    <label for="last_name" class="col-md-12 col-form-label">Email</label>\
+                    <div class="col-md-12">\
+                        <input id="last_name" type="text" class="form-control" name="email" value="' + values.email + '" required autofocus>\
+                    </div>\
+                </div>';
+
+                $('#updateBody').html(updateData);
+            });
+        });
+
+        $('.updateUser').on('click', function () {
+            var values = $(".users-update-record-model").serializeArray();
+            var postData = {
+                name: values[0].value,
+                email: values[1].value,
+            };
+
+            var updates = {};
+            updates['/Users/' + updateID] = postData;
+
+            firebase.database().ref().update(updates);
+
+            $("#update-modal").modal('hide');
+        });
+
+        // Remove Data
+        $("body").on('click', '.removeData', function () {
+            var id = $(this).attr('data-id');
+            $('body').find('.users-remove-record-model').append('<input name="id" type="hidden" value="' + id + '">');
+        });
+
+        $('.deleteRecord').on('click', function () {
+            var values = $(".users-remove-record-model").serializeArray();
+            var id = values[0].value;
+            firebase.database().ref('Users/' + id).remove();
+            $('body').find('.users-remove-record-model').find("input").remove();
+            $("#remove-modal").modal('hide');
+        });
+        $('.remove-data-from-delete-form').click(function () {
+            $('body').find('.users-remove-record-model').find("input").remove();
+        });
+    </script>
+//techsolutionstuff
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
 </body>
 
 </html>
